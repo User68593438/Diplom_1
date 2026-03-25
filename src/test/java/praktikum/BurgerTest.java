8aa40c7652d093708f9b43b50c8a91407b227f02
Olya Muhortova <Lelishna-M@yandex.ru>
2026-03-26 00:59:24 +0400

    Добавлены тесты на методы: getReceipt getPrice; Создан отчет jacoco

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -1,6 +1,5 @@
 package praktikum;
 
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +8,9 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 
@@ -28,37 +30,80 @@ public class BurgerTest {
     @Before
     public void setUp() {
         burger = new Burger();
+        bun = mock(Bun.class);
+        ingredientSauce = mock(Ingredient.class);
+        ingredientFilling = mock(Ingredient.class);
+
+        // Создать стабы для переменных теста "Получить рецепт и его чек"
+        // Булка
+        when(bun.getName()).thenReturn("Black Bun");
+        when(bun.getPrice()).thenReturn(100f);
+
+        // Соус
+        when(ingredientSauce.getName()).thenReturn("Cheesy");
+        when(ingredientSauce.getPrice()).thenReturn(50f);
+        when(ingredientSauce.getType()).thenReturn(IngredientType.SAUCE);
+
+        // Начинка
+        when(ingredientFilling.getName()).thenReturn("Chicken");
+        when(ingredientFilling.getPrice()).thenReturn(150f);
+        when(ingredientFilling.getType()).thenReturn(IngredientType.FILLING);
     }
 
-    // Тест положить булку для бургера
+    // Тест "Положить булку для бургера"
     @Test
     public void setBunsTest() {
-        burger.setBuns(bun); // Добавили булку
-        Assert.assertEquals("Ожидаем другую булку", bun, burger.bun ); // Проверяем что булка добавилась
+        burger.setBuns(bun); // Добавить булку
+        Assert.assertEquals("Ожидаем другую булку", bun, burger.bun ); // Проверить что булка добавилась
     }
 
-    // Тест добавить ингредиент в бургер
+    // Тест "Добавить ингредиент в бургер"
     @Test
     public void addIngredientTest() {
-        burger.addIngredient(ingredientSauce); // Добавили ингредиент
-        Assert.assertEquals("Ожидаем другой ингредиент", List.of(ingredientSauce), burger.ingredients); // Проверяем что ингредиент добавился
+        burger.addIngredient(ingredientSauce); // Добавить ингредиент
+        Assert.assertEquals("Ожидаем другой ингредиент", List.of(ingredientSauce), burger.ingredients); // Проверить что ингредиент добавился
     }
 
-    // Тест удалить ингредиент из бургера
+    // Тест "Удалить ингредиент из бургера"
     @Test
     public void removeIngredientTest() {
-        burger.addIngredient(ingredientFilling); // Добавили ингредиент
-        burger.removeIngredient(0); // Удалили ингредиент
-        Assert.assertTrue("Ингредиент не удалили", burger.ingredients.isEmpty()); // Проверяем что ингредиент удалился
+        burger.addIngredient(ingredientFilling); // Добавить ингредиент
+        burger.removeIngredient(0); // Удалить ингредиент
+        Assert.assertTrue("Ингредиент не удалили", burger.ingredients.isEmpty()); // Проверить что ингредиент удалился
     }
 
-    // Тест переместить ингредиенты в бургере
+    // Тест "Переместить ингредиенты в бургере"
     @Test
     public void moveIngredientTest() {
-        burger.addIngredient(ingredientFilling); //
-        burger.addIngredient(ingredientSauce);
-        burger.moveIngredient(1,0);
-        Assert.assertEquals("Первым ингредиентом должен стать соус", ingredientSauce, burger.ingredients.get(0));
+        burger.addIngredient(ingredientFilling); // Добавить начинку
+        burger.addIngredient(ingredientSauce); // Добавить соус
+        burger.moveIngredient(1,0); // Поменять местами ингредиенты
+        Assert.assertEquals("Первым ингредиентом должен стать соус", ingredientSauce, burger.ingredients.get(0)); // Проверить что ингредиенты поменялись местами
+    }
+
+    // Тест "Получить рецепт и его чек"
+    @Test
+    public void getReceiptTest() {
+        burger.setBuns(bun); // Добавить булку
+        burger.addIngredient(ingredientSauce); // Добавить соус
+        burger.addIngredient(ingredientFilling); // Добавить начинку
+
+        String actualReceipt = burger.getReceipt(); // Фактический результат
+
+        // Создать ожидаемый результат
+        String expectedReceipt =
+                String.format("(==== %s ====)%n", bun.getName()) +
+                        String.format("= %s %s =%n", ingredientSauce.getType().toString().toLowerCase(),
+                                ingredientSauce.getName()) +
+                        String.format("= %s %s =%n", ingredientFilling.getType().toString().toLowerCase(),
+                                ingredientFilling.getName()) +
+                        String.format("(==== %s ====)%n", bun.getName()) +
+                        String.format("%nPrice: %f%n", burger.getPrice());
+        System.out.println("Receipt:");
+        System.out.println(burger.getReceipt());
+
+        // Проверить что результаты совпадают
+        Assert.assertEquals("Pезультаты не совпадают",expectedReceipt, actualReceipt);
     }
 
 }
